0af1602a90448d28251173fb349e5d7575498c22
youme <mrsimplelife@naver.com>
2021-03-12 23:25:19 +0900

    타입스크립트 장점

---
+++ why-ts/sample.js
@@ -0,0 +1,5 @@
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(10, "20"));
+var result = sum(10, 20);
---
+++ why-ts/sample.ts
@@ -0,0 +1,6 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+// add(10, "20");
+var result2 = add(10, 20);
+result2.toLocaleString();
