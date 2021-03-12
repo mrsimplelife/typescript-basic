aa266a56a7a66b7e799fdb5e4fba1406ebfc2b72
youme <mrsimplelife@naver.com>
2021-03-12 23:28:23 +0900

    Add ts-check

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,5 +1,16 @@
+// function sum(a, b) {
+//   return a + b;
+// }
+// console.log(sum(10, "20"));
+// var result = sum(10, 20);
+// @ts-check
+/**
+ *
+ * @param {number} a 첫번째숫자
+ * @param {number} b 두번째숫자
+ * @returns
+ */
 function sum(a, b) {
   return a + b;
 }
-console.log(sum(10, "20"));
-var result = sum(10, 20);
+sum(10, "20");
